5264f8b2eb7a7f810fbfd36e43e1bde76032ff5a
aabasinejad <a.abasinejad@gmail.com>
2019-01-27 19:22:30 +0100

    update

--- Network_Analysis.py
+++ Network_Analysis.py
@@ -43,22 +43,30 @@ def create_graph(union_adjacency, sgi_adjacency, I_adjacency):
     G_sgi = nx.from_pandas_adjacency(sgi_adjacency)
     G_sgi.name = 'SGI Graph'
     
-    # making complete graph of intersection data_set
+    # making complete graph of Intersection data_set
     G_I = nx.from_pandas_adjacency(I_adjacency)
     G_I.name = 'Intersection interactions Graph'
     
-    return G_union, G_sgi, G_I
+    # making large connected component of union graph
+    G_lcc_union = max(nx.connected_component_subgraphs(G_union), key=len)
+    G_lcc_union.name = 'Large Connected Component of G_union'
+    
+    G_lcc_I = max(nx.connected_component_subgraphs(G_I), key=len)
+    G_lcc_I.name = 'Large Connected Component of G_I'
+    
+    return G_union, G_sgi, G_I, G_lcc_union, G_lcc_I
 
-def large_connected_component():
+
+# lcc: Large Connected Component
+#def lcc():   
     
     
+def clustering():
     
-    
-def network_measures():
     
 def save_results():
-    
-    f = open('Network_Analysis_results.txt', 'w')
+    G_union, G_sgi, G_I, G_lcc_union, G_lcc_I = create_graph(union_adjacency, sgi_adjacency, I_adjacency)
+    f = open('Network_meesures.txt', 'w')
     
     ### Results of G_Union
     f.write('Union_complete_graph details....\n')
@@ -91,7 +99,7 @@ def save_results():
     
     # The average degree connectivity is the average nearest neighbor degree of nodes with degree k. 
     avg_deg_sgi = nx.average_degree_connectivity(G_sgi)
-    avg_deg_sgi_res = open('average_degree_union.txt', 'w')
+    avg_deg_sgi_res = open('average_degree_sgi.txt', 'w')
     avg_deg_sgi_res.write('\t'.join(['#degree k','average connectivity'])+ '\n')
     for k, v in avg_deg_sgi.items():
         avg_deg_sgi_res.write('\t'.join(map(str,([k,v])))+ '\n')
@@ -112,7 +120,7 @@ def save_results():
     
     # The average degree connectivity is the average nearest neighbor degree of nodes with degree k. 
     avg_deg_I = nx.average_degree_connectivity(G_I)
-    avg_deg_I_res = open('average_degree_union.txt', 'w')
+    avg_deg_I_res = open('average_degree_Intersection.txt', 'w')
     avg_deg_I_res.write('\t'.join(['#degree k','average connectivity'])+ '\n')
     for k, v in avg_deg_I.items():
         avg_deg_I_res.write('\t'.join(map(str,([k,v])))+ '\n')
@@ -123,6 +131,50 @@ def save_results():
     f.write('Network diameter: ' + str(nx.diameter(G_I))+ '\n')
     # The radius is the minimum eccentricity.
     f.write('Network radius: ' + str(nx.radius(G_I)) + '\n')
+    
+    f.close()
+    
+    
+    f_lcc = open('lcc_Network_measures.txt', 'w')
+    f_lcc.write('#### Large Connected Component Graphs ####\n')
+    
+    # Results of G_lcc_union
+    f_lcc.write('Union_lcc_complete_graph details....\n')
+    f_lcc.write(nx.info(G_lcc_union))
+    f_lcc.write('Average_path_length: '+ str(nx.average_shortest_path_length(G_lcc_union))+ '\n')
+    
+    # The average degree connectivity is the average nearest neighbor degree of nodes with degree k. 
+    avg_deg_union_lcc = nx.average_degree_connectivity(G_lcc_union)
+    avg_deg_unionlcc_res = open('average_degree_Union_lcc.txt', 'w')
+    avg_deg_unionlcc_res.write('\t'.join(['#degree k','average connectivity'])+ '\n')
+    for k, v in avg_deg_union_lcc.items():
+        avg_deg_unionlcc_res.write('\t'.join(map(str,([k,v])))+ '\n')
+    avg_deg_unionlcc_res.close()
+    
+    f_lcc.write('Average clustering coefficient: '+ str(nx.average_clustering(G_lcc_union))+ '\n')
+    # The diameter is the maximum eccentricity.
+    f_lcc.write('Network diameter: ' + str(nx.diameter(G_lcc_union))+ '\n')
+    # The radius is the minimum eccentricity.
+    f_lcc.write('Network radius: ' + str(nx.radius(G_lcc_union)) + '\n')
+    
+    # Results of G_lcc_I
+    f_lcc.write('Intersection_lcc_complete_graph details....\n')
+    f_lcc.write(nx.info(G_lcc_I))
+    f_lcc.write('Average_path_length: '+ str(nx.average_shortest_path_length(G_lcc_I))+ '\n')
+    
+    # The average degree connectivity is the average nearest neighbor degree of nodes with degree k. 
+    avg_deg_I_lcc = nx.average_degree_connectivity(G_lcc_I)
+    avg_deg_I_lcc_res = open('average_degree_Intersection_lcc.txt', 'w')
+    avg_deg_I_lcc_res.write('\t'.join(['#degree k','average connectivity'])+ '\n')
+    for k, v in avg_deg_I_lcc.items():
+        avg_deg_I_lcc_res.write('\t'.join(map(str,([k,v])))+ '\n')
+    avg_deg_I_lcc_res.close()
+    
+    f_lcc.write('Average clustering coefficient: '+ str(nx.average_clustering(G_lcc_I))+ '\n')
+    # The diameter is the maximum eccentricity.
+    f_lcc.write('Network diameter: ' + str(nx.diameter(G_lcc_I))+ '\n')
+    # The radius is the minimum eccentricity.
+    f_lcc.write('Network radius: ' + str(nx.radius(G_lcc_I)) + '\n')
     
     f.close()
     
